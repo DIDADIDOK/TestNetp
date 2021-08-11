74908873a744d92522daae36e7ed2ba6569638bd
DIDADIDOK <70018396+DIDADIDOK@users.noreply.github.com>
2021-08-12 00:29:15 +0300

    Update Test.py

--- Test.py
+++ Test.py
@@ -9,7 +9,7 @@ class NetpeakTest():
         tabs = driver.window_handles
         driver.switch_to_window(tabs[num])
 
-    def btnLog(self, id, keys):
+    def btn_log(self, id, keys):
         btn = driver.find_element_by_id(id)
         btn.clear()
         btn.send_keys(keys)
@@ -23,19 +23,19 @@ def main():
     driver.find_element_by_link_text('Команда').click()
     driver.find_element_by_partial_link_text('Стать частью команды').click()
     netp.pool(1)
-    btnGreen = driver.find_element_by_link_text('Я хочу работать в Netpeak')
-    btnGreenPush = EC.element_to_be_clickable(btnGreen)
+    btn_green = driver.find_element_by_link_text('Я хочу работать в Netpeak')
+    btn_green_push = EC.element_to_be_clickable(btn_green)
     netp.pool(0)
     driver.find_element_by_class_name('custom-link').click()
     netp.pool(2)
-    netp.btnLog('login', 'Boba')
-    netp.btnLog('password', 'Aboba')
-    btnDis = driver.find_element_by_xpath('//*[@id="loginForm"]/div[5]/button/span')
-    btnDisPush = EC.element_to_be_clickable(btnDis)
+    netp.btn_log('login', 'Boba')
+    netp.btn_log('password', 'Aboba')
+    btn_dis = driver.find_element_by_xpath('//*[@id="loginForm"]/div[5]/button/span')
+    btn_dis_push = EC.element_to_be_clickable(btn_dis)
     driver.find_element_by_xpath('//*[@id="loginForm"]/div[4]/div/md-checkbox/div[1]').click()
-    btnDis.click()
+    btn_dis.click()
     bulka = EC.visibility_of_element_located((By.XPATH, '/md-toast/div'))
-    logRed = driver.find_elements_by_class_name('input-container md-input-invalid')
+    log_red = driver.find_elements_by_class_name('input-container md-input-invalid')
 
 if __name__ == "__main__":
     main()
